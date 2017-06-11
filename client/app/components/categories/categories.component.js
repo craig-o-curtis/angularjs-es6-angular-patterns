148381de6bff8438be8d53cf6aabca0c643f4e4d
Craig Curtis <ccurtis@unicon.net>
2017-06-11 14:53:44 -0700

    Change categories.controller to original tutorial code

--- client/app/components/categories/categories.component.js
+++ client/app/components/categories/categories.component.js
@@ -2,11 +2,10 @@ import template from './categories.html';
 import controller from './categories.controller';
 import './categories.styl';
 
-// use ES6 shorthand syntax for object initializers
-const CategoriesComponent = {
-    template,
-    controller,
-    controllerAs: 'categoriesListCtrl'
-}
+const categoriesComponent = {
+  template,
+  controller,
+  controllerAs: 'categoriesListCtrl'
+};
 
-export default CategoriesComponent;
+export default categoriesComponent;
